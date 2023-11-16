d8b45e93e1de68fe72d6e7a2575c6671b315ed34
enamul.shamim <enamul.shamim@trucklagbe.com>
2023-11-16 18:29:31 +0600

    mutex added

--- mutex/mutex.go
+++ mutex/mutex.go
@@ -6,41 +6,25 @@ import (
 	"time"
 )
 
-func isEven(n int) bool {
-	return n%2 == 0
+func add(n int) int {
+	fmt.Println("add called!")
+	return n + 1
 }
-
+func print(n int) {
+	time.Sleep(1 * time.Second)
+	fmt.Println("n ", n)
+}
 func main() {
+	n := 1
 	var m sync.Mutex
-	n := 0
-	m.Lock()
-	defer m.Unlock()
 
-	// goroutine 1
-	// reads the value of n and prints true if its even
-	// and false otherwise
-	go func() {
-		nIsEven := isEven(n)
-		// we can simulate some long running step by sleeping
-		// in practice, this can be some file IO operation
-		// or a TCP network call
-		time.Sleep(5 * time.Millisecond)
-		if nIsEven {
-			fmt.Println(n, " is even")
-			return
-		}
-		fmt.Println(n, "is odd")
-	}()
-
-	// goroutine 2
-	// modifies the value of n
-	time.Sleep(1 * time.Millisecond)
 	go func() {
 		m.Lock()
 		defer m.Unlock()
-		n++
+		print(n)
 	}()
+	fmt.Println("before add call()")
+	n = add(n)
+	time.Sleep(2 * time.Second)
 
-	// just waiting for the goroutines to finish before exiting
-	time.Sleep(1 * time.Second)
 }
